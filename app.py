a4ab36505a81e7ede961ad0c06cb73bbc0be3df5
Brandon <bbergeron.trumpet@gmail.com>
2023-10-16 19:46:45 -0400

    fix time

--- app.py
+++ app.py
@@ -215,7 +215,7 @@ def closeAllDrivers(drivers: dict):
 
 schedule.every().day.at("08:40:00", "US/Eastern").do(createDriverDict, numDrivers)
 schedule.every().day.at("08:41:30", "US/Eastern").do(prepareDrivers, driverContainer)
-schedule.every().day.at("08:58:58", "US/Eastern").do(fire, driverContainer)
+schedule.every().day.at("08:59:58", "US/Eastern").do(fire, driverContainer)
 schedule.every().day.at("09:30:00", "US/Eastern").do(closeAllDrivers, driverContainer)
 
 
